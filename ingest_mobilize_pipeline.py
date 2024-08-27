e6c34c60575718ee7312e8ed2d3589e49e4c5fa1
Crystal <crystaljgong@gmail.com>
2024-08-27 19:59:51 -0400

    Upload into gcs instead of local, refactor transformation

--- ingest_mobilize_pipeline.py
+++ ingest_mobilize_pipeline.py
@@ -1,53 +1,96 @@
+"""Ingests Event Attendances data from Mobilize API and loads the data into bigquery."""
 import requests
 from google.cloud import bigquery
 import os
+import logging
+import smart_open
+import json
+from datetime import datetime, timezone
 
-
-def download_data() -> json[list[dict]]:
-    base_url = "https://api.mobilize.us/v1/"
+def download_attendances_data() -> json[list[dict]]:
+    """
+    Send a get request to the mobilize API's Attendances endpoint and retrieves all data from attendances endpoint
+    """
+    base_url = "https://api.mobilize.us/v1/"    
     endpoint = "attendances"
     headers = {"Authorization": "Bearer {}".format(os.environ.get("MOBILIZE_API_KEY"))}
 
     response = requests.get(base_url + endpoint, headers=headers)
+
     result = response.json
+
+    if result["error"]:
+        logging.error(f"Failed to retrieve attendances data with error: {result["error"]}")
+    
     return result
 
+def extract_event_data(row: dict) -> dict:
+    """Takes a row of Mobilize API data and returns a dict of specific key:value pairs for that event.
+    
+    Args:
+        row: a dict of events from the Mobilize API json response
+    """
+    event = {
+        key: value
+        for key, value in row["event"].items()
+        if key
+        in (
+            "created_date",
+            "modified_date",
+            "id",
+            "title",
+            "event_type",
+            "summary",
+            "description",
+        )
+    }
+    return event
 
-def save_data(data: list[dict]) -> str:
-    fp = "data/attendances.json"
-    f = open(filepath, "w")
-    import json
 
-    json.dump(data, f, indent=4)
+def save_data_to_gcs(data: list[dict], filepath: str) -> str:
+    """
+    Takes a json of event data, transforms data into rows, and saves to google cloud storage with the given filepath.
 
+    Args:
+        data: a list of attendance objects from the Mobilize attendances API
+        filepath: the filepath to write to in GCS, relative to 'gs://mobilize/events/{filepath}'
+    """
 
-def load_events(filepath: str):
-    file = open("data/attendances.json", "r")
-    data = file.read()
+    with smart_open.open(f'gs://mobilize/events/{filepath}', 'wb') as fout:
+        for row in data:
+            try:
+                fout.write(extract_event_data(row))
+            except Exception as e:
+                logging.error(f"Error loading row {row}, with error {e}")
 
-    for row in data:
+def load_events_to_bigquery(filepath: str):
+    """
+    Stream events data from a filepath in google cloud storage into bigquery events table
+    
+    Args:
+        filepath: the filepath to read from in GCS, relative to 'gs://mobilize/events/{filepath}'
+    """
+    client = bigquery.Client()
+    table = client.get_table("wfp-data-project.mobilize.events")
+
+    # stream from GCS
+    for event in smart_open(f'gs://mobilize/events/{filepath}'):
         try:
-            client = bigquery.Client()
-            table = client.get_table("wfp-data-project.mobilize.events")
-            event = {
-                key: value
-                for key, value in row["event"].items()
-                if key
-                in (
-                    "created_date",
-                    "modified_date",
-                    "id",
-                    "title",
-                    "event_type",
-                    "summary",
-                    "description",
-                )
-            }
             client.insert_rows(table, [event])
-        except:
-            print("error loading row")
+        except Exception as e:
+            logging.error(f"Error loading row into bigquery. Row: {event}")
 
 
-data = download_data()
-filepath = save_data(data)
-loadevents(filepath)
+timestamp = datetime.now(timezone.utc)
+
+response_json = download_attendances_data()
+data = response_json["data"]
+
+# Only try loading data if the API returned any data.
+if data:
+    filepath = f"attendances/{timestamp}.json"
+
+    data_csv = extract_event_data()
+
+    save_data_to_gcs(data_csv, filepath)
+    load_events_to_bigquery(filepath)
